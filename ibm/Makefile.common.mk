a6fcb156cb56d8cb8ca1110909840412fbfa73ed
pp <przemyslaw.pytel@ibm.com>
2022-01-10 13:11:55 +0100

    add bundle build at the end

--- ibm/Makefile.common.mk
+++ ibm/Makefile.common.mk
@@ -79,7 +79,6 @@ MANIFEST_TOOL_ARGS ?=
 endif
 
 images: $(MANIFEST_TOOL)
-	@make build-bundle-image
 ifeq ($(BUILD_LOCALLY),1)
 	@make build.all BUILDX_ARGS=--push
 	@$(MANIFEST_TOOL) $(MANIFEST_TOOL_ARGS) push from-args --platforms linux/amd64,linux/ppc64le,linux/s390x --template $(DOCKER_REGISTRY)/$(IMAGE_NAME):$(VERSION)-ARCH --target $(DOCKER_REGISTRY)/$(IMAGE_NAME):$(VERSION) || $(FAIL)
@@ -94,3 +93,4 @@ else
 	@$(MANIFEST_TOOL) $(MANIFEST_TOOL_ARGS) push from-args --platforms linux/amd64,linux/ppc64le,linux/s390x --template $(DOCKER_REGISTRY)/$(IMAGE_NAME)-controller:$(VERSION)-ARCH --target $(DOCKER_REGISTRY)/$(IMAGE_NAME)-controller:$(VERSION) || $(FAIL)
 	@$(MANIFEST_TOOL) $(MANIFEST_TOOL_ARGS) push from-args --platforms linux/amd64,linux/ppc64le,linux/s390x --template $(DOCKER_REGISTRY)/$(IMAGE_NAME)-controller:$(VERSION)-ARCH --target $(DOCKER_REGISTRY)/$(IMAGE_NAME)-controller:$(VERSION)-$(GIT_VERSION) || $(FAIL)
 endif
+	@make build-bundle-image
